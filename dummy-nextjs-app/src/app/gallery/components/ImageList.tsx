3ac3bfff10f4d329b974e96cafa0abd26c4246b0
KasperOmari <mohammad.omari852@gmail.com>
2024-12-02 05:08:04 +0300

    Fix npm run build failure on eslint and other <img> warning

--- dummy-nextjs-app/src/app/gallery/components/ImageList.tsx
+++ dummy-nextjs-app/src/app/gallery/components/ImageList.tsx
@@ -1,4 +1,5 @@
 import { config } from "@/app/config/config";
+import Image from "next/image";
 import { FC } from "react";
 
 interface ImageListCompProps {
@@ -9,7 +10,7 @@ export const ImageListComp:FC<ImageListCompProps> = ({imageList}) => {
     return <div className="flex flex-wrap justify-center items-center gap-4 p-4">
         {imageList.map((imageId) => (
             <div key={imageId} className="w-full md:w-1/3 lg:w-1/4 p-2 flex justify-center">
-                <img className="rounded-lg shadow-lg object-cover w-full h-full max-w-xs"
+                <Image className="rounded-lg shadow-lg object-cover w-full h-full max-w-xs"
                     src={`${config.getImageUrl}${imageId}`}
                     loading="lazy"
                     alt="Generated thumbnail"
