f0657ea5b5c6ba3ba1fc71158ae5a75a8e6059f8
KasperOmari <mohammad.omari852@gmail.com>
2024-12-03 19:03:27 +0300

    Move contants to environment variables

--- dummy-nextjs-app/src/app/gallery/components/ImageList.tsx
+++ dummy-nextjs-app/src/app/gallery/components/ImageList.tsx
@@ -1,5 +1,4 @@
 import { config } from "@/app/config/config";
-import Image from "next/image";
 import { FC } from "react";
 
 interface ImageListCompProps {
@@ -10,7 +9,7 @@ export const ImageListComp:FC<ImageListCompProps> = ({imageList}) => {
     return <div className="flex flex-wrap justify-center items-center gap-4 p-4">
         {imageList.map((imageId) => (
             <div key={imageId} className="w-full md:w-1/3 lg:w-1/4 p-2 flex justify-center">
-                <Image className="rounded-lg shadow-lg object-cover w-full h-full max-w-xs"
+                <img className="rounded-lg shadow-lg object-cover w-full h-full max-w-xs"
                     src={`${config.getImageUrl}${imageId}`}
                     loading="lazy"
                     alt="Generated thumbnail"
--- dummy-nextjs-app/src/app/globalConstants.tsx
+++ dummy-nextjs-app/src/app/globalConstants.tsx
@@ -1,2 +1,2 @@
-export const NEXT_PUBLIC_BACKEND_URL = "http://62.146.182.67:1337"
-export const NEXT_PUBLIC_GENERATE_IMAGE_WEBHOOK_URL = "https://cloud.activepieces.com/api/v1/webhooks/kRceRAJbzaUwmqQ1vLTDF/sync"
+export const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+export const NEXT_PUBLIC_GENERATE_IMAGE_WEBHOOK_URL = process.env.NEXT_PUBLIC_GENERATE_IMAGE_WEBHOOK_URL!;
